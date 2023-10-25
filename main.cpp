e98b24412e8f4c69826145b724f6921429741a2e
asbernal <125429785+asbernal@users.noreply.github.com>
2023-10-24 21:30:25 -0500

    Add files via upload
    Working on the iterative way of computing the number of scalar multiplications.

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+void fillMatricesData();
+void zeroSpread(int arr[][5])
+{
+    for(int x = 0; x < 5; x++)
+    {
+        arr[x][x] = 0;
+    }
+}
+void oneSpread(int _k, int _i, int _j, int arr[][5], int pArr[6])  // int arr** to pass 2d pointer
+{
+    
+    
+}
+
+int main(){
+
+    /*           {
+        M[i,j] =  0: if i = j
+                  min or max or avg: { M[i,j] = M[ i, k ] + M[ k+1, j ] + p_i-1 * p_k * p_j }
+                 }
+    */
+
+    int MATRICES = 5;
+    int scalarMatrix[5][5] = {
+                                {-1,-1,-1,-1,-1},
+                                {-1,-1,-1,-1,-1},
+                                {-1,-1,-1,-1,-1},
+                                {-1,-1,-1,-1,-1},
+                                {-1,-1,-1,-1,-1}
+                             };
+
+    int M1[] = {9,3};
+    int M2[] = {3,9};
+    int M3[] = {9,1};
+    int M4[] = {1,8};
+    int M5[] = {8,4};
+
+    int p0 = 9;
+    int p1 = 3;
+    int p2 = 9;
+    int p3 = 1;
+    int p4 = 8;
+    int p5 = 4;
+
+    int p[] = {9, 3, 9, 1, 8, 4};
+
+    zeroSpread(scalarMatrix);
+
+    int spread = 1; //spread
+
+    for(int sp = 1; sp < MATRICES; sp++)
+    {
+        for(int i = 1; i < MATRICES - sp + 1; i++)
+        {
+            int j = i + sp - 1;
+            int k = i;
+            for(j; j < MATRICES; j = j + sp)
+            {
+                int temp = scalarMatrix[i][k] + scalarMatrix[k+1][j] + (p[i-1] * p[k] * p[j]);
+                cout << temp << endl;
+                
+            }
+            return -1;
+        }
+    }
+    return 0;
+}
+
+void fillMatricesData(){
+    cout << "Enter number of Matrices: " << endl;
+    //
+    cout << "Enter dimensions for: " << endl;
+}
